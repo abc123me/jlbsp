357c4c7b55a6bd51f6a4aabcf381c3cf07bf2c07
jeremiah <jeremiahbl@protonmail.com>
2025-12-20 01:38:34 -0600

    ntp: add preliminary http server

---
+++ board/ntp/skeleton/www/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>NTP server</title>
+		<link rel="stylesheet" href="style.css">
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<p>Test123!</p>
+		<?php echo "php works!"; ?>
+	</body>
+</html>
