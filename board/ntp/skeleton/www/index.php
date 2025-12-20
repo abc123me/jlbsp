40004c6a21fa0f23107a87f21d0da514ec8374bd
jeremiah <jeremiahbl@protonmail.com>
2025-12-20 01:45:22 -0600

    ntp-www: add jquery

--- board/ntp/skeleton/www/index.php
+++ board/ntp/skeleton/www/index.php
@@ -3,6 +3,7 @@
 		<title>NTP server</title>
 		<link rel="stylesheet" href="style.css">
 		<script src="script.js"></script>
+		<script src="jquery-3.7.1.js"></script>
 	</head>
 	<body>
 		<p>Test123!</p>
