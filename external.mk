5540d72d946eb29a7448e7572dbf7878acc699b5
jeremiah <jeremiahbl@protonmail.com>
2025-12-20 02:51:40 -0600

    jlbsp: add deploy make target

--- external.mk
+++ external.mk
@@ -1,1 +1,4 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_JLBSP_PATH)/packages/*/*.mk))
+
+deploy:
+	$(BR2_EXTERNAL_JLBSP_PATH)/support/deploy.sh
---
+++ support/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+build="$(basename $BASE_DIR)"
+
+. "${BR2_EXTERNAL_JLBSP_PATH}/bsps/defaults"
+. "${BR2_EXTERNAL_JLBSP_PATH}/bsps/${build}.bsp"
+
+if [ -n "$DEPLOY_TO" ]; then
+	addr="${DEPLOY_USER}@${DEPLOY_TO}"
+	scp "${BASE_DIR}/${DEPLOY_FILE}" "${addr}:${DEPLOY_LOCATION}"
+	ssh "${addr}" "${DEPLOY_COMMAND}" "${DEPLOY_LOCATION}"
+else
+	echo "Build is not deployable, please configure in ${build}.bsp"
+	exit 1
+fi
