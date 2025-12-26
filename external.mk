75e67d59ee180543c1a8a14b9f7690bc98f00217
jeremiah <jeremiahbl@protonmail.com>
2025-12-25 22:06:08 -0600

    external.mk: make all requirement for deploy, add reconfigure target

--- external.mk
+++ external.mk
@@ -1,4 +1,7 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_JLBSP_PATH)/packages/*/*.mk))
 
-deploy:
+deploy: all
 	$(BR2_EXTERNAL_JLBSP_PATH)/support/deploy.sh
+
+reconfigure:
+	$(BR2_EXTERNAL_JLBSP_PATH)/bsp setup $(notdir $(BASE_DIR))
