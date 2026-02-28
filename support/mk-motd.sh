1f2571469c6d97785285efd8190eb9fe7184267f
jeremiah <jeremiahbl@protonmail.com>
2026-02-28 00:19:01 -0600

    mk-motd.sh: add fpga md5 to motd

--- support/mk-motd.sh
+++ support/mk-motd.sh
@@ -9,8 +9,10 @@ welcome="Welcome to ${bsp} bsp © Jeremiah Lowe $year-$((${year}+1))"
 cnt="$(echo "$welcome" | wc -c)"
 cnt=$((cnt+4))
 divider="$(for i in $(seq 1 $cnt); do printf "─"; done)"
+fpga="${BR2_EXTERNAL_JLBSP_PATH}/board/${bsp}/fpga/fpga.bit"
 
 file="${TARGET_DIR}/etc/motd"
+
 echo -e "${divider}" >>"$file"
 echo -e "   ${welcome}" >>"$file"
 echo -e "${divider}" >>"$file"
@@ -18,6 +20,9 @@ echo -e "   Built by:  ${USER}@$(hostname)"                             >>"$file
 echo -e "   Built on:  $(date)"                                         >>"$file"
 echo -e "   Buildroot: ${BR2_VERSION_FULL}"                             >>"$file"
 echo -e "   JLBSP:     jlbsp${BR2_EXTERNAL_JLBSP_VERSION} $branch"      >>"$file"
+if [ -n "$fpga" ]; then
+	echo -e "   FPGA MD5:  $(md5sum "$fpga" | awk "{print \$1}")"           >>"$file"
+fi
 echo -e "${divider}" >>"$file"
 
 echo "MOTD created at ${file}:"; cat "$file"
