1efd252aaa4acb770c6b74072c33c4297cd19807
jeremiah <jeremiahbl@protonmail.com>
2026-03-04 22:26:13 -0600

    fbgl-rs.mk: fix build issues by bypassing --offline flag

--- packages/fbgl-rs/fbgl-rs.mk
+++ packages/fbgl-rs/fbgl-rs.mk
@@ -8,4 +8,9 @@ FBGL_RS_VERSION = master
 FBGL_RS_SOURCE = fbgl-rs-$(FBGL_RS_VERSION).tar.gz
 FBGL_RS_SITE = $(call github,abc123me,fbgl-rs,$(FBGL_RS_VERSION))
 
+define FBGL_RS_CARGO_FETCH
+    cd $(@D) &&  $(HOST_MAKE_ENV) $(PKG_CARGO_ENV) cargo fetch
+endef
+FBGL_RS_PRE_BUILD_HOOKS += FBGL_RS_CARGO_FETCH
+
 $(eval $(cargo-package))
