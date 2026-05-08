dda2187d669b32c7b99e9f05d4533338fe6fcdf8
jeremiah <jeremiahbl@protonmail.com>
2026-05-08 02:22:30 -0500

    jlbsp: add ddmafb driver

---
+++ packages/driver-ddmafb/driver-ddmafb.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# driver-ddmafb
+#
+################################################################################
+
+DRIVER_DDMAFB_VERSION = master
+DRIVER_DDMAFB_SOURCE = ddmafb-$(DRIVER_DDMAFB_VERSION).tar.gz
+DRIVER_DDMAFB_SITE = $(call github,abc123me,ddmafb,$(DRIVER_DDMAFB_VERSION))
+DRIVER_DDMAFB_INSTALL_STAGING = YES
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
