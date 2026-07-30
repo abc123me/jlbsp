2982c17f410d156c97d3bb53c7d81041b0061487
jeremiah <jeremiahbl@protonmail.com>
2026-07-30 02:20:17 -0500

    driver-ddmafb: pin to version 1.1

--- packages/driver-ddmafb/driver-ddmafb.mk
+++ packages/driver-ddmafb/driver-ddmafb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DRIVER_DDMAFB_VERSION = master
+DRIVER_DDMAFB_VERSION = v1.1
 DRIVER_DDMAFB_SOURCE = ddmafb-$(DRIVER_DDMAFB_VERSION).tar.gz
 DRIVER_DDMAFB_SITE = $(call github,abc123me,ddmafb,$(DRIVER_DDMAFB_VERSION))
 DRIVER_DDMAFB_INSTALL_STAGING = YES
